e908ebfee1d74d1793f3d26d58dc7be0c5d37427
najmul <mdnajmulislam10992@gmail.com>
2025-03-05 01:40:18 +0600

    complete sum range and even checking

---
+++ loop/even.js
@@ -0,0 +1,6 @@
+let i=1;
+while(i<=10){
+    if(i%2==0) console.log(i+' is even')
+        ++i;
+    
+}
---
+++ loop/sum.js
@@ -0,0 +1,6 @@
+let i=1,sum=0;
+while(i<=10){
+    sum+=i;
+    console.log(sum)
+    ++i
+}

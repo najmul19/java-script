f7b7fb5748f0f1719559ef7bb33218f36903c281
najmul <mdnajmulislam10992@gmail.com>
2025-03-04 17:42:02 +0600

    completed the array pop push and shift

---
+++ array/push_pop.js
@@ -0,0 +1,14 @@
+const numbers = [12,22,13,14];
+const ages=[]
+
+console.log(numbers)
+numbers.push(10)//push the eleement to add a new index to end of the previous array
+numbers.push(69,20,19,23,59)
+console.log(numbers)
+numbers.pop()///remove the last element from the array
+numbers.pop()
+numbers.pop()
+console.log(numbers)
+const ok = numbers.pop()
+console.log('pop value: '+ok)
+console.log('after pop the value the new array is: '+numbers)
---
+++ array/shift.js
@@ -0,0 +1,6 @@
+const numbers = [12,22,13,14];
+console.log(numbers)
+numbers.shift() // pop to first
+console.log(numbers)
+numbers.unshift(69) // add to first
+console.log(numbers)

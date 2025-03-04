da31a93898279c28e19f0a28411af149153e73cc
najmul <mdnajmulislam10992@gmail.com>
2025-03-04 18:34:24 +0600

    complete the includes,clice indexOf and check is Array

---
+++ array/basic_array_methods.js
@@ -0,0 +1,15 @@
+const numbers = [12,22,13,14];
+const nw=[3,5,5];
+console.log(numbers.includes(12));
+console.log(numbers.includes(19));
+console.log(numbers.indexOf(19));
+console.log(numbers.indexOf(13));
+
+const nums = [];
+const f=[];
+const ck=1;
+console.log(Array.isArray(nums))
+console.log(Array.isArray(f))
+console.log(Array.isArray(ck))
+console.log(nw.concat(numbers))
+console.log(numbers.slice(2,4))// from 2 to 3 index

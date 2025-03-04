fc8f6b75d08e1f023a35db93c36d79da8c79b9f1
najmul <mdnajmulislam10992@gmail.com>
2025-03-04 17:27:14 +0600

    complete assigning array

---
+++ array/array.js
@@ -0,0 +1,11 @@
+// const num1=12
+// const num2=122
+// const num3=123
+// const num4=124
+
+const numbers = [12,22,13,14];
+console.log(numbers)
+const f=['abul', 'mokbul']
+console.log(f)
+const mix=['abul', true, 2,'mokbul']
+console.log(mix)

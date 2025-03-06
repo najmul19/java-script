dcc81f40b3ea8487dbab65e9be8bf5cd3b8eb97a
najmul <mdnajmulislam10992@gmail.com>
2025-03-07 00:23:48 +0600

    complete the string vs array

---
+++ string/string.js
@@ -0,0 +1,11 @@
+const country = 'bangladesh'
+const div = 'ok oo'
+const dis = `oskid`
+const tha = new String('dns') //object
+//string is immutable
+//aray is mutable
+console.log(country)
+
+country[0]='s'
+
+console.log(country)

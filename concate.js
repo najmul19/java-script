0b27a65fcd3e394552c5e93b92bdc503e5032754
najmul <mdnajmulislam10992@gmail.com>
2025-03-10 12:41:31 +0600

    complete string concate split and slice

---
+++ concate.js
@@ -0,0 +1,9 @@
+const f ='najmul'
+const s ='islam'
+const full = f +' '+ s
+console.log(full)
+
+const full2 = f.concat(' ').concat(s)
+console.log(full2)
+
+console.log(s.includes('i'))
---
+++ slice.js
@@ -0,0 +1,16 @@
+const a ='ajkjsodkfk'
+const p = a.slice(2,5)
+console.log(p)
+
+const s = 'I am good a working person'
+console.log(s.split(' '))
+console.log(s.split('a'))
+const f = 'ok, sdjk, skd, kjsadj, kd'
+
+const ff = f.split(',')
+console.log(ff)
+
+const rf=[ 'ok', ' sdjk', ' skd', ' kjsadj', ' kd' ]
+console.log(rf.join())
+console.log(rf.join('|'))
+console.log(rf.join('-'))

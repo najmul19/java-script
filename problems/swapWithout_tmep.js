27a618b4838b1f078299d667d67f73f0acc5cf28
najmul <mdnajmulislam10992@gmail.com>
2025-03-10 16:34:08 +0600

    added swap woth temp and without temp which is destructing

---
+++ problems/swapWithout_tmep.js
@@ -0,0 +1,17 @@
+let a=10;
+let b=19;
+console.log(a,b)
+let c;
+c=a;
+a=b;
+b=c;
+
+console.log(a,b)
+
+// ----------------------
+let x=4;
+let y= 2;
+
+console.log(x,y);
+[x,y]=[y,x];
+console.log(x,y)

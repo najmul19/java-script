b9af474847f5b1f15a5d624d1a45637d957178ee
najmul <mdnajmulislam10992@gmail.com>
2025-03-10 15:03:47 +0600

    learn about return function with argument and parameter

---
+++ function/return_func.js
@@ -0,0 +1,13 @@
+function TenTimes(number){
+    const res=number*10
+    return res;
+    console.log(res)
+}
+function cutHalf(number) {
+    const half=number/2
+}
+
+// TenTimes(5) wrong
+console.log(TenTimes(4))
+const res = TenTimes(5);
+console.log(res)

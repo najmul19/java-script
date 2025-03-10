bfa5449dbb69953dc127f25aedfdbc6eeb3d8a8e
najmul <mdnajmulislam10992@gmail.com>
2025-03-10 14:53:11 +0600

    learn about parameter

---
+++ function/parameter.js
@@ -0,0 +1,10 @@
+// normal
+function vatKha(){
+    console.log('ruja maiya din kom kha')
+}
+vatKha();
+
+function add(a,b){ // parameter
+    console.log(a+b)
+}
+add(2,4) // function call with argument

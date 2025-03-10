8e91a8f712338fdadbc3851973e87e3c29648506
najmul <mdnajmulislam10992@gmail.com>
2025-03-10 13:18:42 +0600

    complete propert

---
+++ string/propertt.js
@@ -0,0 +1,17 @@
+const p={
+    name:'najmul',
+    age:23,
+    profession:'enginner',
+    salary:0,
+    marride:false,
+    'fav place':['uganda','panama','gana'],
+
+}
+console.log(p)
+// dot notation
+console.log(p.name)
+
+// bracket notation
+console.log(p['age'])
+// console.log(p.fav place) wrong
+console.log(p['fav place']) 
---
+++ string/value.js
@@ -0,0 +1,15 @@
+const p={
+    name:'najmul',
+    age:23,
+    profession:'enginner',
+    salary:0,
+    marride:false,
+    'fav place':['uganda','panama','gana'],
+
+}
+p.salary=10000000
+p['marride']=true
+console.log(p)
+
+const keyName = 'profession'
+console.log(p[keyName])

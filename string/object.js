b190c66fb6e8ed20d28f0da47914a5e38faf2d30
najmul <mdnajmulislam10992@gmail.com>
2025-03-10 13:05:52 +0600

    learn basic object declaration premitive and non premitive

---
+++ string/object.js
@@ -0,0 +1,27 @@
+// premitive, primary
+const age = 21;
+const schole = 'kejdio jasa'
+const isPas = true
+let isDev;
+const sub=['bang','eng','math']
+const bottle_wrong=['white',45]
+
+
+//object
+// non-premitive
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean:false,
+}
+const subject={
+    name:'bangla',
+    teache:'nj',
+    examD:'30 feb',
+    chapter:['firsr','second','third'],
+    exams:{
+        name:'final exam',
+        marks:40
+    }
+}

bb0d8eb04be8a76bb7bdcc2e6806b4bfb911b330
najmul <mdnajmulislam10992@gmail.com>
2025-03-10 16:16:17 +0600

    added some math fucntionality

---
+++ problems/math.js
@@ -0,0 +1,16 @@
+const min=Math.min(45,1,3,5,-3,6,5)
+console.log(min)
+const max=Math.max(45,1,3,5,-3,6,5)
+console.log(max)
+console.log(Math.PI)
+console.log(Math.abs(-88))
+console.log(Math.abs(5-10))
+console.log(Math.floor(5.9))
+console.log(Math.round(5.5))
+console.log(Math.round(5.4))
+console.log(Math.ceil(5.4))
+console.log(Math.random()) // 0-1 random value
+console.log(Math.random()*10) // 0-10 random value
+const rand = Math.round(Math.random()*10) // 0-10 random value
+
+console.log(rand)

e2dcb0cb8c6c69ccbfa78a9296f1069a4d4ff985
najmul <mdnajmulislam10992@gmail.com>
2025-03-10 14:01:51 +0600

    learn more about array travarsal

---
+++ array/looping.js
@@ -0,0 +1,20 @@
+/**
+ * looping thecknique
+ * for loop
+ * while loop
+ * do while
+ * for of-->array
+ * for in-->object
+ */
+const f = ['slkklkd','skdmlk','jskj']
+
+for( let i of f){
+    console.log(i)
+    
+}
+
+let len = f.length
+for( let i=0;i<len;++i) {
+    console.log(i);
+    console.log(f[i]);
+}

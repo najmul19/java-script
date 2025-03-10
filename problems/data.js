4fd9059db2e93b74abab332509bd77ab10dd5a29
najmul <mdnajmulislam10992@gmail.com>
2025-03-10 16:25:32 +0600

    added date related functionality

---
+++ problems/data.js
@@ -0,0 +1,15 @@
+const today = new Date()
+const date= new Date('2062-10-19')
+
+console.log(date)
+console.log(date.getDay())
+console.log(date.getMonth())
+const specificDate = new Date(2091,0,26)
+console.log(specificDate)
+specificDate.setMonth(10)
+console.log(specificDate)
+console.log(specificDate.toLocaleString())
+console.log(specificDate.toLocaleString('en-GB'))
+
+// unisx epoc
+

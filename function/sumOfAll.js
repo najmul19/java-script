3019afbc7e8b380202d3bcd3b8e70956ed83a0b0
najmul <mdnajmulislam10992@gmail.com>
2025-03-10 15:26:01 +0600

    learn more about function

---
+++ function/sumOfAll.js
@@ -0,0 +1,17 @@
+// objective: sum of all numbers
+
+function sumOfAllNumbers(num){
+    console.log(num)
+    let sum=0;
+    for(let i of num){
+         sum+=i
+    }
+    return sum;
+}
+
+
+
+
+const nums=[1,2,3,4,5,6,7,8,9]
+const sum = sumOfAllNumbers(nums);
+console.log('sum f numbers is', sum)
